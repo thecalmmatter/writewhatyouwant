f00d27869d4c77398d114d37b9b41cb28c4b2a73
Prasoon Majumdar <prasoons@Prasoons-MacBook-Pro.local>
2025-02-18 10:51:41 +0530

    modifying changes

--- linkedin_blog_flask.py
+++ linkedin_blog_flask.py
@@ -19,7 +19,7 @@ planner = Agent(
     role="Content Planner",
     goal="Plan engaging and factually accurate content on {topic}",
     backstory=(
-        "You're responsible for planning a linkedin article on {topic}. "
+        "You're responsible for planning a New York Times article on {topic}. "
         "You collect information that helps the audience learn and make informed decisions. "
         "Your work serves as the foundation for the Content Writer."
     ),
@@ -43,9 +43,9 @@ writer = Agent(
 
 editor = Agent(
     role="Editor",
-    goal="Edit the Linkedin post to align with journalistic standards in the style of Joe Rogan or lex fridman or Chamath Palihapitiya, add some interesting icons and the brand’s voice.",
+    goal="Edit the New York Times post to align with journalistic standards and add some interesting icons and the brand’s voice.",
     backstory=(
-        "As an editor, you refine the Linkedin post for clarity, grammar, structure and balance. "
+        "As an editor, you refine the New York Times post for clarity, grammar, structure and balance. "
         "Your focus is on journalistic integrity, ensuring the piece remains professional and unbiased."
     ),
     allow_delegation=False,
@@ -55,7 +55,7 @@ editor = Agent(
 
 hook_generator = Agent(
     role="Hook Generator",
-    goal="Add a Linkedin post hook.",
+    goal="Add a New York Times post hook.",
     backstory=(
         "As a hook generator, you add the hook for quirkiness, compelling messages, and irresistible curiosity that makes people stop scrolling and dive in."
     ),
@@ -67,7 +67,7 @@ hook_generator = Agent(
 # Define Tasks
 plan = Task(
     description=(
-        "1. Research and prioritize the latest trends, key players, and noteworthy news on {topic}.\n"
+        "1. Deep research and prioritize the latest trends, key players, and noteworthy news on {topic}.\n"
         "2. Identify the target audience, considering their interests and pain points.\n"
         "3. Develop a detailed content outline including an introduction, key points, and a call to action.\n"
         "4. Include SEO keywords and relevant data or sources."
@@ -78,25 +78,25 @@ plan = Task(
 
 write = Task(
     description=(
-        "1. Use the content plan to craft a compelling 400 word linkedin post on {topic}.\n"
+        "1. Use the content plan to craft a compelling 400 word New York Times post on {topic}.\n"
         "2. Naturally integrate SEO keywords.\n"
         "3. Structure sections/subtitles engagingly.\n"
         "4. Ensure the post has an engaging introduction, insightful body, and strong conclusion.\n"
         "5. Proofread for grammatical accuracy and alignment with the brand's voice."
     ),
-    expected_output="A well-structured, engaging linkedin  post in markdown format, ready for publication.",
+    expected_output="A well-structured, engaging New York Times  post in markdown format, ready for publication.",
     agent=writer
 )
 
 edit = Task(
-    description="Proofread and refine the linkedin post for clarity, grammar, structure, and tone consistency.",
-    expected_output="A polished linkedin post in markdown format, ready for publication.",
+    description="Proofread and refine the New York Times post for clarity, grammar, structure, and tone consistency.",
+    expected_output="A polished New York Times post in markdown format, ready for publication.",
     agent=editor
 )
 
 hook = Task(
-    description="Craft an engaging, quirky, and compelling hook for a LinkedIn post that grabs attention instantly. The hook should spark curiosity, encourage engagement, and set the stage for the main content.",
-    expected_output="A sharp, well-structured LinkedIn post hook in markdown format, ensuring clarity, grammatical accuracy, and an engaging tone, ready for publication.",
+    description="Craft an engaging, quirky, and compelling hook for a New York Times post that grabs attention instantly. The hook should spark curiosity, encourage engagement, and set the stage for the main content.",
+    expected_output="A sharp, well-structured New York Times post hook in markdown format, ensuring clarity, grammatical accuracy, and an engaging tone, ready for publication.",
     agent=hook_generator
 )
 
